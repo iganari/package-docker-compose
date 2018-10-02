06458d56b84f6c120e262fa7263407979193d250
iganari <cigaguri@gmail.com>
2018-10-02 09:56:39 +0900

    ラッパーの修正

--- 10_nginx-loadbalancer/build.sh
+++ 10_nginx-loadbalancer/build.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-# docker-compose stop
-# docker-compose rm -f
+docker-compose stop
+docker-compose rm -f
 docker-compose build --no-cache
 docker-compose up -d --remove-orphans
 docker-compose ps
