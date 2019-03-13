6ccc9cda7290cb512e02fff14024264d432eded2
iganari <cigaguri@gmail.com>
2019-03-13 10:42:55 +0900

    update docker script

--- 10_nginx-loadbalancer/dcs.sh
+++ 10_nginx-loadbalancer/dcs.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
+# ref: https://gist.github.com/iganari/5bd2c315d5705545c10b998ec56da43e
 
-set -x
+# set -x
 
-docker-compose stop
-docker-compose rm -f
-docker-compose build --no-cache
-docker-compose up -d --remove-orphans
-docker-compose ps
+# See how we were called
+case "${1}" in
+    start)
+        echo "docker-compose up"
+        docker-compose up -d
+        ;;
+    stop)
+        echo "docker-compose stop"
+        docker-compose stop
+        ;;
+    down)
+        echo "docker-compose down"
+        docker-compose down
+        ;;
+    build)
+        echo "docker-compose build"
+        docker-compose build
+        ;;
+    status)
+        echo "docker-compose status"
+        docker-compose ps
+        ;;
+    *)
+        echo "Usage: $(basename $0) {start|stop|down|build|status}"
+        exit 1
+esac
